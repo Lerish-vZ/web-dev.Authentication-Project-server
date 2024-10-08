8600d0548f4319d8283042fcf1496a7a193f4f16
Lerish-vZ <lerishavz@gmail.com>
2024-10-08 13:51:42 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-require("dotenv").config;
+require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
 
 //const PORT = 4000;
