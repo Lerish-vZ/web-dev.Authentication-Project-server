d24b940a38fcc088cf6302e8cba07f363d582a37
Lerish-vZ <lerishavz@gmail.com>
2024-10-08 13:22:15 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const PORT = 4000;
+require("dotenv").config;
+const { MONGO_URL, PORT } = process.env;
+
+//const PORT = 4000;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB is connected successfully"))
+.catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
