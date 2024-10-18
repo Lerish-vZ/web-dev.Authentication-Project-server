de1ab09ae5e0e92217ca63b0aa864154b393366d
Lerish-vZ <lerishavz@gmail.com>
2024-10-18 10:37:51 +0200

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -7,23 +7,13 @@ const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
 const { MONGO_URL, PORT } = process.env;
 
-
-console.log("MONGO_URL:", MONGO_URL);
-console.log("PORT:", PORT);
-
 mongoose
   .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-)
+  })
   .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
-
-  mongoose.connection.on('error', err => {
-    console.error('MongoDB connection error:', err);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
