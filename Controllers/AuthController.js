a0092ac34458a33ea0cf90d2bb798a42b2f1be11
Lerish-vZ <lerishavz@gmail.com>
2024-10-21 16:05:11 +0200

    update

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -15,7 +15,7 @@ module.exports.Signup = async (req, res, next) => {
       withCredentials: true,
       httpOnly: false,
     });
-    // console.log(token);
+    console.log(token);
     res
       .status(201)
       .json({ message: "User signed in successfully", success: true, user });
