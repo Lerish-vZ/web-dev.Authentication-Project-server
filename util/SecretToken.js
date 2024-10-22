bfa8f515004a6ba6eb7bb0ecc45af5bdf629a6c9
Lerish-vZ <lerishavz@gmail.com>
2024-10-22 08:13:45 +0200

    update

--- util/SecretToken.js
+++ util/SecretToken.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
+  console.log("Token Key:", process.env.TOKEN_KEY);
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
