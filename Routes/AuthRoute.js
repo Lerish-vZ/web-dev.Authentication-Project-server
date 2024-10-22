631f24ab722d4867f54aa8b04e0a0391bb8e97ad
Lerish-vZ <lerishavz@gmail.com>
2024-10-22 08:15:16 +0200

    update

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -2,8 +2,8 @@ const { Signup, Login } = require("../Controllers/AuthController");
 const { userVerification } = require("../Middleware/AuthMiddleware");
 const router = require("express").Router();
 
+router.post('/', userVerification);
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post('/', userVerification);
 
 module.exports = router;
